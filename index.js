0636217806320dcac96a8ccbd8c06d9147dc76ee
joynul24 <joynula919@gmail.com>
2026-06-12 00:10:00 +0600

    vercel.json file added

--- index.js
+++ index.js
@@ -454,7 +454,4 @@ app.get("/", (req, res) => {
   res.send("🚀 Volunteer Server Running Successfully");
 });
 
-// Server start
-app.listen(port, () => {
-  console.log(`🚀 Server running on port ${port}`);
-});
+export default app;
