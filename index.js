6380bf75020149b70a7ca5a2fa8d06ffa32a7a44
joynul24 <joynula919@gmail.com>
2026-06-12 10:16:48 +0600

    frontend url added cors origin function

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:5173", "https://volunteer-server-smfv.vercel.app"],
+  origin: [
+    "http://localhost:5173",
+    "https://volunteer-frontend-one.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"]
+  credentials: true
 }));
 app.use(express.json());
 
